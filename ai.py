1439ac6c775bd1ca4182336570e1f5965ec903d0
FrhnFS <123940359+FrhnFS@users.noreply.github.com>
2024-07-26 18:10:12 +0700

    ai.py

--- ai.py
+++ ai.py
@@ -1,11 +1,9 @@
 # Import necessary libraries
-import streamlit as st
-import numpy as np
-import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, classification_report
+streamlit
+numpy
+pandas
+scikit-learn
+
 
 # Title and header
 st.title('Klasifikasi Penyakit Jantung')
