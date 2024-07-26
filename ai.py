f832775f031219c550c958d273205e8bf7ee2f6d
FrhnFS <123940359+FrhnFS@users.noreply.github.com>
2024-07-26 18:12:54 +0700

    ai.py

--- ai.py
+++ ai.py
@@ -1,8 +1,11 @@
 # Import necessary libraries
-streamlit
-numpy
-pandas
-scikit-learn
+import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, classification_report
 
 
 # Title and header
